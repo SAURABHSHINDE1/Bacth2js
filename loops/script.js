d430ab7bec11eb3cfb10a2e491f7561de00daa13
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2026-01-09 12:17:57 +0530

    9 jan 2026

---
+++ loops/script.js
@@ -0,0 +1,81 @@
+// for loop
+
+// for(initializtion: condition ; increamnet/decreament){
+
+// }
+
+// for(let i = 1 ; i <= 10 ; i++){
+//     console.log(i)
+// }
+
+// 1
+// 2
+
+// for(let i = 10 ; i >= 1; i--){
+//     console.log("hello")
+// }
+
+
+// let arr = [10,35,56,78,99,34,89,78,67,55]
+
+// console.log(arr.length)
+
+// for(let i = 0; i < arr.length ; i++){
+
+//         console.log(arr[i])
+
+// }
+
+// for(let i =0)
+
+// console.log(arr[0]) //1
+// console.log(arr[1]) //2
+// console.log(arr[2]) //3
+// console.log(arr[3]) //4
+// console.log(arr[4]) //5
+// console.log(arr[5]) //6
+// console.log(arr[6]) //7
+// console.log(arr[7]) //8
+// console.log(arr[8]) //9
+// console.log(arr[9]) //10
+// console.log(arr[10])
+
+
+// for(let i = 0 ; i < arr.length; i++){
+
+//     console.log(arr[i])
+    
+// }
+// i = 0
+// console.log(arr[0])
+
+// i = 1
+//console.log(arr[i])
+
+
+// i = 2
+//console.log(arr[i])
+
+
+// while loop 
+
+// while(condition){
+
+// }
+
+// let arr = [10,35,56,78,99,34,89,78,67,55]
+
+// let i = 0
+
+// while(i < arr.length){
+
+//     console.log(arr[i])
+//     i++
+// }
+
+// let i = 1
+
+// do{
+//     console.log(i)
+//     i++
+// }while(i < 0)
---
+++ operators/script.js
@@ -0,0 +1,132 @@
+// arithmetic operators
+// +
+// -
+// *
+
+// console.log(a + b)
+
+// console.log(a - b)
+
+// console.log(a * b)// /
+// %
+
+// let a = 10
+
+// let b = 30
+
+
+// console.log(a / b)
+
+// console.log( a % b)
+
+
+
+// realational/compairison opeartors
+
+// ==
+// ===
+// !=
+// >
+// <
+// >=
+// <=
+
+// let a = 13
+
+// let b  = 12
+
+// console.log(a == b)
+
+// console.log(a === b)
+
+// console.log(a != b)
+
+// console.log(a > b)
+
+// console.log(a < b)
+
+// console.log(a >= b)
+
+// console.log(a <= b)
+
+
+// Assignment opeartors
+
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+
+// let str = "abc"
+
+// let str1 = str
+
+// console.log(str1)
+
+// let a = "vhjervueru"
+
+// let str = a
+
+// console.log(str)
+
+// let sum = 10
+
+// // sum = sum + 10
+// sum +=10
+
+// console.log(sum)
+
+// let arr = [10,20,30,40,50,60]
+//     let sum = 0
+
+// for(let i = 0 ; i <arr.length ; i++){
+
+//     sum = sum + arr[i]
+
+// }
+
+// console.log(sum)
+
+
+
+// let sum = 10
+
+// sum += 10
+
+// console.log(sum)
+
+// let a  = 21
+
+// // a = a -2
+
+// a -= 2
+
+// console.log(a)
+
+// logical operators
+
+// &&
+
+// ||
+
+// let a = 10
+
+// let b =22
+
+// console.log((a > b) || (b > a))
+
+// let a = 22
+
+// let b = 23
+
+// let c = 22
+
+
+// if(a== b || a == c){
+//     window.location.href = "test.html"
+// }
+// else{
+//     console.log("all are not same")
+// }
