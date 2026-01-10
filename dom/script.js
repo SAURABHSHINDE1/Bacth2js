44dca7fd7864cff08e43354736fac6c100723fa5
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2026-01-10 11:59:59 +0530

    10 jan 2026

---
+++ dom/script.js
@@ -0,0 +1,60 @@
+
+
+// let heading = document.getElementById("test")
+
+// heading.style.color = 'red'
+// heading.style.fontSize = "100px"
+
+// console.log(heading)
+
+// heading.textContent = "hello DreamsGuider"
+
+// heading.innerText = "hello DreamsGuider"
+
+// heading.innerHTML = "<p> hello DreamsGuider </p>"
+
+
+// let image = document.getElementById("img")
+// console.log(image)
+
+// let div  = document.getElementById("text")
+// console.log(div)
+
+// div.style.height ="200px"
+// div.style.width ="200px"
+// div.style.backgroundColor = "orange"
+
+
+// QuerySelector
+
+// let heading = document.querySelector("#test")
+
+// console.log(heading)
+
+// let box = document.querySelector(".box")
+// console.log(box)
+
+// let box = document.getElementsByTagName("div")
+// console.log(box)
+
+// let box = document.getElementsByClassName("box")
+// console.log(box)
+
+// let box = document.querySelectorAll(".box")
+// console.log(box)
+
+
+
+// box[1].style.backgroundColor ="red"
+// box[2].style.backgroundColor ="red"
+// box[3].style.backgroundColor ="red"
+// box[4].style.backgroundColor ="red"
+// box[5].style.backgroundColor ="red"
+// box[6].style.backgroundColor ="red"
+
+// for(let i =0 ; i < box.length ;i++ ){
+
+// box[i].style.backgroundColor ="red"
+// box[i].style.borderRadius = "30px"
+
+// }
