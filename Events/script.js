711cc4bcba09355f1e44b20093dd4b734fae9312
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2026-01-27 11:29:28 +0530

    27 jan 2026

---
+++ Events/script.js
@@ -0,0 +1,35 @@
+
+
+// function Greet(){
+    
+//     alert("hello User")
+
+// }
+
+
+// let btn = document.querySelector(".btn")
+
+// btn.onclick = function(){
+//     alert("hello")
+// }
+
+
+// let btn = document.querySelector("#btn")
+
+// btn.addEventListener("click" , ()=>alert("hello"))
+
+
+// let box = document.querySelector(".box")
+
+// box.addEventListener("click" , ()=>{
+//     window.location.href = "https://www.youtube.com/"
+// })
+
+// const ElementStyle = ()=>{
+
+//     box.style.backgroundColor = "red"
+//     box.style.borderRadius = "20px"
+
+// }
+
+// box.addEventListener("click" , ElementStyle)
---
+++ functions/function.js
@@ -0,0 +1,30 @@
+// function greet(){
+ 
+//     console.log("normal function")
+// }
+
+// // greet()
+
+// const add = ()=>
+//     {
+//         console.log("hello")
+
+//     }
+
+
+// add()
+
+// function (){
+
+// }
+
+
+
+// const greet = ()=>{
+
+//     return 30 + 30
+// }
+
+// let result = greet()
+
+// console.log(result)
