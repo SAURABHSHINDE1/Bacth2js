ba824c00e3e7a094ab7fe5d5eb752d6fef193667
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2026-02-03 17:05:56 +0530

    3 feb 2026

---
+++ arrrayMethods/array.js
@@ -0,0 +1,80 @@
+
+
+// console.log(arr.length)
+
+// push
+// push method is used to add one or more elements in array at the last
+
+// arr.push(34 , 33,22)
+
+// console.log(arr)
+
+// pop
+// pop is used to remove the last element of array
+
+//  arr.pop()
+
+// console.log(arr)
+
+// unshift
+// unshift is used to add one or more elements in array at 1st  postion
+// let arr = [10, 45,67,89, 77,56]
+
+// arr.unshift(55 , 66)
+
+// console.log(arr)
+
+// shift
+// shift is used to  remove element  from the 1st position of the array
+
+// let arr = [10, 45,67,89, 77,56]
+
+// arr.shift()
+
+// console.log(arr)
+
+// splice
+// splice is used to add and remove element  at same time 
+
+// let arr = [10, 45,67,89, 77,56]
+
+// arr.splice(0 , 2)
+
+// console.log(arr)
+
+// slice 
+
+
+// let arr = [10, 45,67,89, 77,56]
+
+// let sliced = arr.slice(0 , 3)
+
+// console.log(sliced)
+
+
+
+// indexof
+
+
+// let arr = [10, 45,67,89, 77 , 67,56]
+
+// let index = arr.indexOf(67)
+
+// console.log(index)
+
+
+// lastIndexOf 
+
+// let arr = [10, 45,67,89, 77 , 67,56 , 67]
+
+// let result = arr.lastIndexOf(64)
+
+// console.log(result)
+
+// includes
+
+// let arr = [10, 45,67,89, 77,56 ,90]
+
+// let result = arr.includes(87)
+
+// console.log(result)
